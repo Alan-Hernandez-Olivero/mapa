5c97a49da66a59ca2a83bd22f4ba573027e52b25
Alan-Hernandez-Olivero <102831982+Alan-Hernandez-Olivero@users.noreply.github.com>
2022-04-01 14:54:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('https://raw.github.com/Alan-Hernandez-Olivero/mapa/blob/main/data.csv')
+st.write(df)
+st.map(df)
