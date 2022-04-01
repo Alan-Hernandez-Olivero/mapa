4843e7f5c4735c4dcf097c525950c6bed24103cc
Alan-Hernandez-Olivero <102831982+Alan-Hernandez-Olivero@users.noreply.github.com>
2022-04-01 14:57:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/Alan-Hernandez-Olivero/mapa/main/data.csv')
-st.write(df)
+st.sidebar.write(df)
 st.map(df)
